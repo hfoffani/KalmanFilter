de26321c093fd2b4134fbdc5df755e3f92820373
hfoffani <hfoffani@gmail.com>
2017-06-02 21:01:24 +0200

    Implement UpdateEKF.
    The error is very big so there is a bug.

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -150,19 +150,17 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
     // Radar updates
-    // double rho = measurement_pack.raw_measurements_[0];
-    // double phi = measurement_pack.raw_measurements_[1];
-    // ekf_.x_ << rho * cos(phi), rho * sin(phi), 0, 0;
-
-    // ekf_.UpdateEKF(ekf_.x_);
+    ekf_.H_ = tools.CalculateJacobian(ekf_.x_);
+    ekf_.R_ = R_radar_;
+    ekf_.UpdateEKF(measurement_pack.raw_measurements_);
   } else {
     // Laser updates
-    // ekf_.x_ << measurement_pack.raw_measurements_[0], measurement_pack.raw_measurements_[1], 0, 0;
+    ekf_.H_ = H_laser_;
     ekf_.R_ = R_laser_;
     ekf_.Update(measurement_pack.raw_measurements_);
   }
 
   // print the output
-  cout << "x_ = " << ekf_.x_ << endl;
-  cout << "P_ = " << ekf_.P_ << endl;
+  // cout << "x_ = " << ekf_.x_ << endl;
+  // cout << "P_ = " << ekf_.P_ << endl;
 }
